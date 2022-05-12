dac70b26312cd98c167162526ac259f9d0d6ba27
juaniandrada23 <juanandrada_07_@hotmail.com>
2022-05-12 16:35:05 -0300

    prueba nueva

--- users/usersController.js
+++ users/usersController.js
@@ -72,6 +72,7 @@ const login = async (req, res, next) => {
       user,
     };
     res.status(200).json({ message: `User ${user.name} Logged in!`, Token_info: tokenData });
+    res.render("login")
   } else {
     let error = new Error();
     error.status = 401;
@@ -79,14 +80,6 @@ const login = async (req, res, next) => {
     next(error);
   }
 };
-
-//LOGIN (POST)
-//mostramos el formulario de recuperación de contraseña
-const ingreso = async (req, res, next) => {
-  const users = req.params.user
-  res.render("ingreso", {users})
-}
-
 
 /*configurar nodemailer*/
 const transport = nodemailer.createTransport({
@@ -160,4 +153,4 @@ const removeOne = async (req, res, next) => {
   dbResponse.affectedRows ? res.status(204).end() : next();
 };
 
-module.exports = { listAll, listOne, register, login, forgot, reset, ingreso, saveNewPass, removeOne, editOne };
+module.exports = { listAll, listOne, register, login, forgot, reset, saveNewPass, removeOne, editOne };
