fc4816355da15418fef96b6773dcb55d661d6ae1
juaniandrada23 <juanandrada_07_@hotmail.com>
2022-05-12 17:21:48 -0300

    ingresar pantalla!

--- users/usersController.js
+++ users/usersController.js
@@ -55,12 +55,6 @@ const register = async (req, res, next) => {
   res.status(201).json({ user: req.body.name, Token_Info: tokenData });
 };
 
-//LOGIN INGRESO (GET)
-//mostramos el formulario de recuperación de contraseña
-const ingreso = (req, res, next) => {
-  res.render("ingreso")
-}
-
 //Login user
 const login = async (req, res, next) => {
   const cleanBody = matchedData(req)
@@ -77,6 +71,7 @@ const login = async (req, res, next) => {
       token: await tokenSign(user, "2h"),
       user,
     };
+    res.render("login");
     res.status(200).json({ message: `User ${user.name} Logged in!`, Token_info: tokenData });
   } else {
     let error = new Error();
@@ -158,4 +153,4 @@ const removeOne = async (req, res, next) => {
   dbResponse.affectedRows ? res.status(204).end() : next();
 };
 
-module.exports = { listAll, listOne, register, login, forgot, ingreso, reset, saveNewPass, removeOne, editOne };
+module.exports = { listAll, listOne, register, login, forgot, reset, saveNewPass, removeOne, editOne };
--- users/usersRoute.js
+++ users/usersRoute.js
@@ -1,5 +1,5 @@
 /*En este archivo pondremos la referencia a la ruta asociada y, si las hubiera, subrutas (por ejemplo, podríamos recibir todas las peticiones de /users, pero también de /users/otracosa... etc). Luego de recibir la petición diferenciando el verbo HTTP, enviaríamos al controlador apropiado. Si hubiera middlewares, se aplicarían en este archivo, entre la petición y el controlador*/
-const { listAll, listOne, register, login, forgot, ingreso, reset, saveNewPass, removeOne, editOne } = require("./usersController")
+const { listAll, listOne, register, login, forgot, reset, saveNewPass, removeOne, editOne } = require("./usersController")
 const router = require("express").Router()
 const { validatorCreateUser, validatorResetPassword, validatorLoginUser } = require("../validators/users");
 const fileUpload = require("../utils/handleStorage");
@@ -15,9 +15,8 @@ router.get("/:id", listOne);
 router.post("/register", fileUpload.single("file"), validatorCreateUser, register)
 
 //Login user
+router.get("/login", login)
 router.post("/login", validatorLoginUser, login)
-
-router.get("/ingreso", ingreso) // mostramos el formulario de inicio de sesion 
 
 //Forgot password
 router.post("/forgot-password", forgot) //desde el front entra el mail del usuario quen olvidó password
@@ -26,7 +25,6 @@ router.get("/reset/:token", reset) //mostramos el formulario de recuperación de
 router.post("/reset/:token", validatorResetPassword, saveNewPass)//recibimos la nueva contraseña desde el formulario
 //patch user
 router.patch("/:id", editOne)
-
 
 //delete user by ID
 router.delete("/:id", removeOne)
